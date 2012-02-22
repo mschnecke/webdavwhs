81056af3ebceb67b8dd910340ac1faabdebdf489
mschnecke <mschnecke@users.noreply.github.com>
2012-02-22 08:38:57 +0100

    - Added: CA Create process; - Added: CA to install needed Server Role features;

--- WebDavWhs.CustomAction/CustomAction.cs
+++ WebDavWhs.CustomAction/CustomAction.cs
@@ -8,18 +8,37 @@ namespace WebDavWhs.CustomAction
 	/// </summary>
 	public class CustomActions
 	{
+		/// <summary>
+		/// Creates the process.
+		/// </summary>
+		/// <param name="session">The session.</param>
+		/// <returns>Custom Action result.</returns>
 		[CustomAction]
-		public static ActionResult TestCa(Session session)
+		public static ActionResult CreateProcess(Session session)
 		{
+			string application = session.CustomActionData["App"];
+			string arguments = session.CustomActionData["Args"];
+
+			session.Log(@"CreateProcess CustomActionData Argument: {0}: '{1}'", "App", application);
+			session.Log(@"CreateProcess CustomActionData Argument: {0}: '{1}'", "Args", arguments);
+
+			string stdoutput = null;
+
 			try
 			{
-				return ActionResult.Success;
+				ProcessHelper.StartProcess(application, arguments, true, true, out stdoutput);
 			}
-			catch(Exception exception)
+			catch (Exception exception)
 			{
-				session.Log(@"Exception: {0}", exception.ToString());
+				session.Log(@"CreateProcess Exception: {0}", exception.ToString());
 				return ActionResult.Failure;
 			}
+			finally
+			{
+				session.Log(@"CreateProcess output: {0}", stdoutput);
+			}
+
+			return ActionResult.Success;
 		}
 	}
 }
---
+++ WebDavWhs.CustomAction/ProcessHelper.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+
+namespace WebDavWhs.CustomAction
+{
+	/// <summary>
+	/// Provides methods to create processes.
+	/// </summary>
+	internal class ProcessHelper
+	{
+		/// <summary>
+		/// 	Occurs when StartProcess output data has been received.
+		/// </summary>
+		public static event DataReceivedEventHandler StartProcessOutputDataReceived;
+
+		/// <summary>
+		/// 	Starts process and waits for exit or runs silently optional.
+		/// </summary>
+		/// <param name = "application">Application to start.</param>
+		/// <param name = "commandLine">Command line to pass.</param>
+		/// <param name = "wait">Wait for exit.</param>
+		/// <param name = "silent">Run silently.</param>
+		public static void StartProcess(string application, string commandLine, bool wait, bool silent)
+		{
+			Process process = new Process();
+
+			try
+			{
+				process.StartInfo.FileName = application;
+				process.StartInfo.Arguments = commandLine;
+				process.StartInfo.ErrorDialog = true;
+				process.StartInfo.UseShellExecute = false;
+				process.StartInfo.RedirectStandardOutput = true;
+				process.OutputDataReceived += OnProcessOutputHandler;
+
+				if(silent)
+				{
+					process.StartInfo.CreateNoWindow = true;
+					process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+				}
+
+				process.Start();
+				process.BeginOutputReadLine();
+
+				if(wait)
+				{
+					process.WaitForExit();
+				}
+			}
+			finally
+			{
+				process.Close();
+			}
+		}
+
+		/// <summary>
+		/// 	Starts process and waits for exit.
+		/// </summary>
+		/// <param name = "application">Application to start.</param>
+		/// <param name = "commandLine">Command line to pass.</param>
+		/// <param name = "silent">Start silent.</param>
+		/// <param name = "wait">Wait for exit.</param>
+		/// <param name = "stdOutput">The standard output.</param>
+		public static void StartProcess(string application, string commandLine, bool silent, bool wait, out string stdOutput)
+		{
+			Process process = new Process();
+			stdOutput = string.Empty;
+
+			try
+			{
+				process.StartInfo.FileName = application;
+				process.StartInfo.Arguments = commandLine;
+				process.StartInfo.ErrorDialog = true;
+				process.StartInfo.UseShellExecute = false;
+				process.StartInfo.RedirectStandardOutput = true;
+
+				if(silent)
+				{
+					process.StartInfo.CreateNoWindow = true;
+					process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+				}
+
+				process.Start();
+				stdOutput = process.StandardOutput.ReadToEnd();
+
+				if(wait)
+				{
+					process.WaitForExit();
+				}
+			}
+			finally
+			{
+				process.Close();
+				process.Dispose();
+			}
+		}
+
+		/// <summary>
+		/// 	Processes the output handler.
+		/// </summary>
+		/// <param name = "sender">The sender.</param>
+		/// <param name = "e">The <see cref = "System.Diagnostics.DataReceivedEventArgs" /> instance containing the event data.</param>
+		private static void OnProcessOutputHandler(object sender, DataReceivedEventArgs e)
+		{
+			if(StartProcessOutputDataReceived != null)
+			{
+				StartProcessOutputDataReceived(null, e);
+			}
+		}
+	}
+}
