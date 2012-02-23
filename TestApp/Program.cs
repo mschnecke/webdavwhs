b82f6d495df433b6a6f726a30dd94e04b219d604
mschnecke <mschnecke@users.noreply.github.com>
2012-02-23 15:50:38 +0100

    - Added: addin file;

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,7 @@ namespace TestApp
 	{
 		static void Main(string[] args)
 		{
+			
 		}
 	}
 }
