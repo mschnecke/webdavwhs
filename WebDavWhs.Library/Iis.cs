1e84c977ff60b59360c44b3c892a731db6453abc
gwatzke <goewat@users.noreply.github.com>
2012-02-27 08:31:51 +0100

    - Added: methods to set the authentication modes - Added: method to create virtual directory

--- WebDavWhs.Library/Iis.cs
+++ WebDavWhs.Library/Iis.cs
@@ -1,47 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using Microsoft.Web.Administration;
 
 namespace WebDavWhs
 {
 	/// <summary>
-	/// Provides properties and methodes to handle IIS
+	/// 	Provides properties and methodes to handle IIS
 	/// </summary>
 	public class Iis : IDisposable
 	{
 		/// <summary>
-		/// Disposable flag.
+		/// 	Disposable flag.
 		/// </summary>
 		private bool isDisposed;
 
 		/// <summary>
-		/// Gets or sets the server manager.
+		/// 	The server manager object
 		/// </summary>
-		/// <value>
-		/// The server manager.
-		/// </value>
+		private ServerManager serverManager;
+
+		/// <summary>
+		/// 	The root path.
+		/// </summary>
+		private const string path = @"/";
+
+		/// <summary>
+		/// 	Gets the server manager.
+		/// </summary>
+		/// <value> The server manager. </value>
 		private ServerManager ServerManager
 		{
-			get;
-			set;
+			get
+			{
+				if(this.serverManager == null)
+				{
+					this.serverManager = new ServerManager();
+				}
+
+				return this.serverManager;
+			}
 		}
 
 		#region Lifecycle
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="Iis"/> class.
+		/// 	Initializes a new instance of the <see cref="Iis" /> class.
 		/// </summary>
 		public Iis()
 		{
 			this.isDisposed = false;
-			this.ServerManager = new ServerManager();
 		}
 
 		/// <summary>
-		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// 	Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
 		/// </summary>
 		public void Dispose()
 		{
@@ -62,79 +73,181 @@ namespace WebDavWhs
 		#endregion
 
 		/// <summary>
-		/// Sets the web dav status.
+		/// 	Creates the virtual directory.
 		/// </summary>
+		/// <param name="virtualDirectoryName"> Name of the virtual directory. </param>
+		/// <param name="physicalPath"> The physical path. </param>
+		public void CreateVirtualDirectory(string virtualDirectoryName, string physicalPath)
+		{
+			Trace.TraceInformation("CreateVirtualDirectory...");
+
+			string defaultWebSite = this.GetDefaultWebSite();
+
+			if(string.IsNullOrEmpty(defaultWebSite))
+			{
+				Trace.TraceError("Default web site not found.");
+				Trace.TraceInformation("CreateVirtualDirectory...finished.");
+				return;
+			}
+
+			if (this.ExistsVirtualDirectory(defaultWebSite, virtualDirectoryName))
+			{
+				Trace.TraceInformation("Virtual directory already exists.");
+				Trace.TraceInformation("CreateVirtualDirectory...finished.");
+				return;
+			}
+
+			Application applicationElement = this.ServerManager.Sites[defaultWebSite].Applications[path];
+			VirtualDirectory virtualDirectory = applicationElement.VirtualDirectories.CreateElement();
+			virtualDirectory.Path = string.Format(@"/{0}", virtualDirectoryName);
+			virtualDirectory.PhysicalPath = physicalPath;
+			applicationElement.VirtualDirectories.Add(virtualDirectory);
+
+			this.ServerManager.CommitChanges();
+			Trace.TraceInformation("CreateVirtualDirectory...finished.");
+		}
+
+		/// <summary>
+		/// Existses the virtual directory.
+		/// </summary>
+		/// <param name="webSiteName">Name of the web site.</param>
+		/// <param name="virtualDirectoryName">Name of the virtual directory.</param>
+		/// <returns>Validation result.</returns>
+		public bool ExistsVirtualDirectory(string webSiteName, string virtualDirectoryName)
+		{
+			Trace.TraceInformation("ExistsVirtualDirectory...");
+
+			VirtualDirectory virtualDirectory =
+					this.ServerManager.Sites[webSiteName].Applications[path].VirtualDirectories[
+						string.Format(@"/{0}", virtualDirectoryName)
+						];
+
+			if (virtualDirectory == null)
+			{
+				Trace.TraceInformation("ExistsVirtualDirectory...finished.");
+				return false;
+			}
+
+			Trace.TraceInformation("ExistsVirtualDirectory...finished.");
+			return true;
+		}
+
+
+		/// <summary>
+		/// 	Sets the windows authentication.
+		/// </summary>
+		/// <param name="webSiteName"> Name of the web site. </param>
+		/// <param name="enabled"> if set to <c>true</c> [enabled]. </param>
+		public void SetWindowsAuthentication(string webSiteName, bool enabled)
+		{
+			Trace.TraceInformation("SetWindowsAuthentication...");
+
+			Configuration configuration = this.ServerManager.GetApplicationHostConfiguration();
+
+			ConfigurationSection windowsAuthenticationSection =
+				configuration.GetSection("system.webServer/security/authentication/windowsAuthentication", webSiteName);
+			windowsAuthenticationSection["enabled"] = enabled;
+
+			this.ServerManager.CommitChanges();
+			Trace.TraceInformation("SetWindowsAuthentication...finished.");
+		}
+
+		/// <summary>
+		/// Sets the anonymous authentication.
+		/// </summary>
+		/// <param name="webSiteName">Name of the web site.</param>
 		/// <param name="enabled">if set to <c>true</c> [enabled].</param>
-		/// <param name="requireSsl">if set to <c>true</c> [require SSL].</param>
+		public void SetAnonymousAuthentication(string webSiteName, bool enabled)
+		{
+			Trace.TraceInformation("SetAnonymousAuthentication...");
+			
+			Configuration configuration = this.ServerManager.GetApplicationHostConfiguration();
+			
+			ConfigurationSection anonymousAuthenticationSection =
+				configuration.GetSection("system.webServer/security/authentication/anonymousAuthentication", webSiteName);
+			
+			anonymousAuthenticationSection["enabled"] = enabled;
+			
+			this.ServerManager.CommitChanges();
+
+			Trace.TraceInformation("SetAnonymousAuthentication...finished.");
+		}
+
+		/// <summary>
+		/// Sets the basic authentication.
+		/// </summary>
+		/// <param name="webSiteName">Name of the web site.</param>
+		/// <param name="enabled">if set to <c>true</c> [enabled].</param>
+		public void SetBasicAuthentication(string webSiteName, bool enabled)
+		{
+			Trace.TraceInformation("SetBasicAuthentication...");
+
+			Configuration configuration = this.ServerManager.GetApplicationHostConfiguration();
+
+			ConfigurationSection basicAuthenticationSection =
+				configuration.GetSection("system.webServer/security/authentication/basicAuthentication", webSiteName);
+				basicAuthenticationSection["enabled"] = enabled;
+
+			this.ServerManager.CommitChanges();
+			Trace.TraceInformation("SetBasicAuthentication...finished.");
+		}
+
+		/// <summary>
+		/// 	Sets the web dav status.
+		/// </summary>
+		/// <param name="enabled"> if set to <c>true</c> [enabled]. </param>
+		/// <param name="requireSsl"> if set to <c>true</c> [require SSL]. </param>
 		public void SetWebDavStatus(bool enabled, bool requireSsl)
 		{
 			Trace.TraceInformation("SetWebDavStatus...");
 
 			string defaultWebSite = this.GetDefaultWebSite();
 
-			if (string.IsNullOrEmpty(defaultWebSite))
+			if(string.IsNullOrEmpty(defaultWebSite))
 			{
 				Trace.TraceError("Default web site not found.");
 				Trace.TraceInformation("SetWebDavStatus...finished.");
 				return;
 			}
-			
-			try
-			{
-				Configuration config = this.ServerManager.GetApplicationHostConfiguration();
-				ConfigurationSection authoringSection = config.GetSection("system.webServer/webdav/authoring", defaultWebSite);
 
-				authoringSection["enabled"] = enabled;
-				authoringSection["requireSsl"] = requireSsl;
-				this.ServerManager.CommitChanges();
-			}
-			finally
-			{
-				Trace.TraceInformation("SetWebDavStatus...finished.");
-			}
+			Configuration config = this.ServerManager.GetApplicationHostConfiguration();
+			ConfigurationSection authoringSection = config.GetSection("system.webServer/webdav/authoring", defaultWebSite);
+
+			authoringSection["enabled"] = enabled;
+			authoringSection["requireSsl"] = requireSsl;
+			this.ServerManager.CommitChanges();
+			Trace.TraceInformation("SetWebDavStatus...finished.");
 		}
 
 		/// <summary>
-		/// Gets the default web site.
+		/// 	Gets the default web site.
 		/// </summary>
-		/// <returns>The default web site.</returns>
-		private string GetDefaultWebSite()
+		/// <returns> The default web site. </returns>
+		public string GetDefaultWebSite()
 		{
 			Trace.TraceInformation("GetDefaultWebSite...");
 			string siteName = string.Empty;
 
-			try
+			Configuration config = this.ServerManager.GetApplicationHostConfiguration();
+			ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
+			ConfigurationElementCollection sitesCollection = sitesSection.GetCollection();
+
+			foreach(ConfigurationElement site in sitesCollection)
 			{
-				Configuration config = this.ServerManager.GetApplicationHostConfiguration();
-				ConfigurationSection sitesSection = config.GetSection("system.applicationHost/sites");
-				ConfigurationElementCollection sitesCollection = sitesSection.GetCollection();
+				long id = (long)site.Attributes["id"].Value;
 
-				foreach (ConfigurationElement site in sitesCollection)
+				if(id != 1)
 				{
-					long id = (long)site.Attributes["id"].Value;
-
-					if (id != 1)
-					{
-						continue;
-					}
-
-					siteName = (string)site.Attributes["name"].Value;
-					break;
+					continue;
 				}
 
-				Trace.TraceInformation("Site name: '{0}'", siteName);
-				return siteName;
+				siteName = (string)site.Attributes["name"].Value;
+				break;
 			}
-			finally
-			{
-				Trace.TraceInformation("GetDefaultWebSite...finished.");
-			}
+
+			Trace.TraceInformation("Site name: '{0}'", siteName);
+			Trace.TraceInformation("GetDefaultWebSite...finished.");
+			return siteName;
 		}
-
-
-
 	}
-
-	
-
 }
